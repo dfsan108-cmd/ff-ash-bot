28ad69f7055a6b8e2ddb7728cd626a0d4c8f0fb1
dfsan108-cmd <dfsan108@gmail.com>
2026-07-20 18:34:46 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
 print("FF ASH BOT")
+from rubpy import Client
+
+bot = Client("BJJAIF0STZGBTRLGEVWBIMBUBJRTNUCIPYBJVSWMNDOMFJISTZUCCINREVFXDVSI")
+
+@bot.on_message()
+async def messages(update):
+    text = update.text
+
+    if text == "/start":
+        await update.reply(
+            "سلام!\n"
+            "برای ثبت نام کلمه register را ارسال کنید."
+        )
+
+    elif text == "register":
+        await update.reply(
+            "ثبت نام شما با موفقیت انجام شد ✅"
+        )
+
+bot.run()
